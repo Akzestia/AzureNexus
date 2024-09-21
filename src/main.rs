b4e801973bc137a4be5793a6467f2dac45afae41
Azure <azurexsx@gmail.com>
2024-09-21 20:20:01 +0200

    Tokio test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use mini_redis::{client, Result};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let mut client = client::connect("127.0.0.1:6379").await?;
+
+    client.set("uxux", "xuxu".into()).await?;
+
+    let result = client.get("uxux").await?;
+
+    println!("got value from the server; result={:?}", result);
+
+    Ok(())
 }
