9cd878f7e3d9707b2295d9c26d10ff1a908c61c5
Azure <azurexsx@gmail.com>
2024-09-26 11:58:39 +0200

    main.rs update

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,31 @@
+use tokio::runtime::Runtime;
 use AzureNexus::server::NexusServer;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let ip = String::from("localhost");
+    let mut config = tun2::Configuration::default();
+    config
+        .address((10, 0, 0, 9))
+        .netmask((255, 255, 255, 0))
+        .destination((10, 0, 0, 1))
+        .up();
 
-    let server = NexusServer::new(6121, &ip);
+    #[cfg(target_os = "linux")]
+    config.platform_config(|config| {
+        config.ensure_root_privileges(true);
+    });
+
+    let rt = Runtime::new()?;
+
+    let async_dev = rt.block_on(async {
+        tun2::create_as_async(&config).expect("Failed to create async TUN device")
+    });
+
+    let mut server = NexusServer::new(6121, ip, async_dev);
+
+    rt.block_on(async {
+        let result = server.listener_start().await;
+    });
+
+    Ok(())
 }
